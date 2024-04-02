40ad737ec0564b47c3499cd59caee2a6bb944dc9
sumaiyaakter <sumaiya.akter.shampa@gmail.com>
2024-04-02 07:19:37 +0600

    add Copy to Clipboard

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const titleOne = document.querySelector(".block h1");
 const titleTwo = document.querySelector(".block h2");
 const currentColor = document.querySelector(".block .color");
+const colorCopy = document.querySelector(".block .copy");
 const btn = document.querySelector('.btn');
 
+const isFirstClick = true;
 
 function generateColor() {
   const hexCode = "abcdef0123456789";
@@ -16,14 +18,26 @@ function generateColor() {
   }
   return hash;
 }
+
 btn.addEventListener("click", () => {
   titleOne.style.color = '#ffffff';
   titleTwo.style.color = '#ffffff';
+  colorCopy.style.color = '#ffffff';
   
   const randomColor = generateColor();
   document.body.style.backgroundColor = randomColor;
   currentColor.style.color = randomColor;
   currentColor.textContent = randomColor;
-  
+
 });
+
+colorCopy.addEventListener('click', () => {
+  if (isFirstClick) {
+    navigator.clipboard.writeText(currentColor.innerText);
+    isFirstClick = false;
+  } else {
+    navigator.clipboard.writeText(currentColor.textContent);
+  }
+});
+
 
